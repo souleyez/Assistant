85a0663f8cf3d1cae5ba1a3808e30df7b66fb044
soulzyn <soulzyn@outlook.com>
2026-04-27 10:44:04 +0800

    Redirect training UI to HTTPS for uploads

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './styles.css'
 
+if (window.location.protocol === 'http:' && ['10.0.121.52', '14.23.49.218'].includes(window.location.hostname)) {
+  window.location.replace(`https://${window.location.host}${window.location.pathname}${window.location.search}${window.location.hash}`)
+}
+
 createApp(App).use(router).mount('#app')
